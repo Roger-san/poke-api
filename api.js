66755d4e72fe1b0a9a5b9ec7edd4e17acf99f506
Roger-san <rogerytal@gmail.com>
2021-04-10 12:49:43 +0200

    as

--- api.js
+++ api.js
@@ -5,7 +5,20 @@ const bodyParser = require("body-parser")
 const api = express()
 
 // CONF CORS
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*") // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+  app.options("*", (req, res) => {
+    // allowed XHR methods
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS")
+    res.send()
+  })
+})
 // CONF DECODE BODYPARSER
 api.use(bodyParser.urlencoded({ extended: true })) //Decodificamos la informacion del body
 
